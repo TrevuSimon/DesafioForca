ca2a6ed39ed16409d48e5e84377b77cdbdc3648d
Guilherme Simão Costa <Gui_SC@outlook.com>
2018-11-12 00:24:03 -0200

    iniciando jogo, e mysql funcionando

--- src/main/java/br/edu/fapi/game/dao/connection/MySqlConnectionProvider.java
+++ src/main/java/br/edu/fapi/game/dao/connection/MySqlConnectionProvider.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnectionProvider {
-    public static String url = "jdbc:mysql://localhost:3306/desafioforca";
+    public static String url = "jdbc:mysql://localhost:3306/desafioforca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     public static String usuario = "root";
     public static String senha = "123456";
 
     public static Connection abrirConexao() throws SQLException {
-
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver ());
         return DriverManager.getConnection(url, usuario, senha);
     }
 }
